e4e6f7cc5d63687fdd101ec8b1c068d7d87f02b5
unknown <g11150621@gmail.com>
2022-05-03 19:42:35 +0800

    Use genus community calculate Alpha diversity to analysis survival

---
+++ Script/WGCNA/4. AlphaDiversitySurvival.R
@@ -0,0 +1,289 @@
+####Eigen genus data####
+workingDir = "D:/Research/Script/WGCNA"
+setwd(workingDir)
+# Load the WGCNA package
+library(WGCNA)
+# The following setting is important, do not omit.
+options(stringsAsFactors = FALSE)
+# Load the expression and trait data saved in the first part
+lnames = load(file = "Genus-01-dataInput.RData")
+# Load network data saved in the second part.
+lnames = load(file = "Genus-02-networkConstruction-stepByStep.RData")
+
+nGenes = ncol(datExpr_g);
+nSamples = nrow(datExpr_g);
+# Recalculate MEs with color labels
+MEs0 = WGCNA::moduleEigengenes(datExpr_g, moduleColors_g)$eigengenes
+MEs = WGCNA::orderMEs(MEs0)
+MEs = MEs[,-10]
+
+####Alpha diversity####
+#  ------->> Loading Raw genus count data--------  
+genus_raw <- read.csv("D:/Research/Data/Raw/Kraken-TCGA-Raw-Data-17625-Samples.csv",header = T,row.names = 1) 
+## Filter the match sample by sample name
+genus_raw <- genus_raw[which(rownames(genus_raw)%in%rownames(MEs)),]
+
+#  ------->> Find community content--------  
+genuscommunity <- cbind(moduleColors_g,moduleLabels_g)
+genuscommunity <- data.frame(genuscommunity)
+genuscommunity$genus <- names(datExpr_g)
+magenta <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="magenta")]
+black <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="black")]
+blue <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="blue")]
+green <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="green")]
+red <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="red")]
+yellow <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="yellow")]
+pink <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="pink")]
+brown <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="brown")]
+turquoise <- genuscommunity$genus[which(genuscommunity$moduleColors_g=="turquoise")]
+#  ------->> Create sub-matrix of genus community--------  
+
+magenta_g <- genus_raw[,which(names(genus_raw)%in%magenta)]
+black_g <- genus_raw[,which(names(genus_raw)%in%black)]
+blue_g <- genus_raw[,which(names(genus_raw)%in%blue)]
+green_g <- genus_raw[,which(names(genus_raw)%in%green)]
+red_g <- genus_raw[,which(names(genus_raw)%in%red)]
+yellow_g <- genus_raw[,which(names(genus_raw)%in%yellow)]
+pink_g <- genus_raw[,which(names(genus_raw)%in%pink)]
+brown_g <- genus_raw[,which(names(genus_raw)%in%brown)]
+turquoise_g <- genus_raw[,which(names(genus_raw)%in%turquoise)]
+#  ------->> Calculate alpha diversity of each community-------- 
+library(vegan)
+magenta_alpha <- vegan::diversity(magenta_g, index = "shannon")
+black_alpha <- vegan::diversity(black_g, index = "shannon")
+blue_alpha <- vegan::diversity(blue_g, index = "shannon")
+green_alpha <- vegan::diversity(green_g, index = "shannon")
+red_alpha <- vegan::diversity(red_g, index = "shannon")
+yellow_alpha <- vegan::diversity(yellow_g, index = "shannon")
+pink_alpha <- vegan::diversity(pink_g, index = "shannon")
+brown_alpha <- vegan::diversity(brown_g, index = "shannon")
+turquoise_alpha <- vegan::diversity(turquoise_g, index = "shannon")
+
+alpha_matrix <- cbind(CAmagenta = magenta_alpha,CAblack = black_alpha,
+                      CAblue = blue_alpha, CAgreen = green_alpha,
+                      CAred = red_alpha,CAyellow = yellow_alpha,
+                      CApink = pink_alpha, CAbrown = brown_alpha,
+                      CAturquoise = turquoise_alpha)
+alpha_matrix <- data.frame(alpha_matrix)
+
+#  ------->> Scatter plot for eigen genus and alpha-diversity-------- 
+##order the dataframes
+MEs <- MEs[order(rownames(MEs)),]
+alpha_matrix <- alpha_matrix[order(rownames(alpha_matrix)),]
+df <- cbind(MEs,alpha_matrix)
+library(ggpubr)
+pdf(file = "C:/Users/user/Desktop/My Plot.pdf",   # The directory you want to save the file in
+    width = 8, # The width of the plot in inches
+    height = 6)
+ggpubr::ggscatter(df, x = "MEmagenta", y = "CAmagenta",
+          color = "#FFB266", size = 3, # Points color, shape and size
+          add = "reg.line",  # Add regressin line
+          add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+          conf.int = TRUE, # Add confidence interval
+          cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+          cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+          title = "Scatter plot for eigen genus and alpha diversity of magenta community",
+          xlab = "Community's Eigen genus",
+          ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEblack", y = "CAblack",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of black community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEblue", y = "CAblue",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of blue community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEgreen", y = "CAgreen",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of green community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEred", y = "CAred",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of red community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEyellow", y = "CAyellow",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of yellow community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEpink", y = "CApink",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of pink community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEbrown", y = "CAbrown",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of brown community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+
+ggpubr::ggscatter(df, x = "MEturquoise", y = "CAturquoise",
+                  color = "#FFB266", size = 3, # Points color, shape and size
+                  add = "reg.line",  # Add regressin line
+                  add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+                  conf.int = TRUE, # Add confidence interval
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman", label.sep = "\n",label.y = 0.7),
+                  title = "Scatter plot for eigen genus and alpha diversity of turquoise community",
+                  xlab = "Community's Eigen genus",
+                  ylab = "Community's Alpha diversity")
+dev.off()
+
+####Survival analysis####
+#  ------->> Load the survival OS time data--------
+#load package
+library("survival")
+library("survminer")
+library("readxl")
+
+#Read data
+tcga <-  read.csv("D:/Research/TCGA/TCGA.csv", header = TRUE)
+tcga_cdr <-  read_excel("D:/Research/Data/Redo/TCGA-CDR-SupplementalTableS1.xlsx",sheet = "TCGA-CDR")
+
+#Grab the 138 sample from TCGA barcode and give sample name
+barcode <- tcga$barcode
+tcga_select <- tcga_cdr[which(tcga_cdr$bcr_patient_barcode%in%barcode),c("bcr_patient_barcode","OS","OS.time")]
+tcga <- tcga[order(tcga$barcode),]
+tcga_select<-tcga_select[order(tcga_select$bcr_patient_barcode),]
+tcga_select$X <- tcga$X
+#cbind tcga and eigen genus data
+tcga_select <- tcga_select[order(tcga_select$X),]
+alpha_matrix <- alpha_matrix[order(rownames(alpha_matrix)),]
+
+ana_table <- cbind(tcga_select,alpha_matrix)
+ana_table$OS <- ifelse(ana_table$OS.time >= 2000, 0,ana_table$OS)
+ana_table$OS.time <- ifelse(ana_table$OS.time >= 2000, 2000, ana_table$OS.time)
+covariates <- names(alpha_matrix)
+univ_formulas <- sapply(covariates,
+                        function(x) as.formula(paste('Surv(OS.time, OS)~', x)))
+
+univ_models <- lapply( univ_formulas, function(x){coxph(x, data = ana_table)})
+# Extract data 
+univ_results <- lapply(univ_models,
+                       function(x){ 
+                         x <- summary(x)
+                         p.value<-signif(x$wald["pvalue"], digits=2)
+                         wald.test<-signif(x$wald["test"], digits=2)
+                         beta<-signif(x$coef[1], digits=2);#coeficient beta
+                         HR <-signif(x$coef[2], digits=2);#exp(beta)
+                         HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+                         HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
+                         HR <- paste0(HR, " (", 
+                                      HR.confint.lower, "-", HR.confint.upper, ")")
+                         res<-c(beta, HR, wald.test, p.value)
+                         names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                         #return(exp(cbind(coef(x),confint(x))))
+                       })
+
+res <- t(as.data.frame(univ_results, check.names = FALSE))
+res <- data.frame(res)
+summ <- res[which(res$p.value<0.05),]
+
+select_community <- rownames(res)
+select_community <- paste(select_community,collapse = "+")
+formular <- as.formula(paste('Surv(OS.time, OS)~', select_community))
+
+res.cox <- coxph(formular, data =  ana_table)
+multiple_result <- summary(res.cox)
+
+test <- survfit(res.cox)
+ggsurvplot(test,data=ana_table, color = "#2E9FDF",
+           ggtheme = theme_minimal())
+
+
+#9 community genus survival plot
+
+library(dplyr)
+new <- ana_table %>% mutate(magenta_exp = ifelse(CAmagenta >=median(ana_table$CAmagenta), "High", "Low"),
+                            black_exp = ifelse(CAblack >=median(ana_table$CAblack), "High", "Low"),
+                            brown_exp = ifelse(CAbrown >= median(ana_table$CAbrown),"High","Low"),
+                            green_exp = ifelse(CAgreen >= median(ana_table$CAgreen),"High","Low"),
+                            red_exp = ifelse(CAred >= median(ana_table$CAred),"High","Low"),
+                            yellow_exp = ifelse(CAyellow >= median(ana_table$CAyellow),"High","Low"),
+                            pink_exp = ifelse(CApink >= median(ana_table$CApink),"High","Low"),
+                            blue_exp = ifelse(CAblue >= median(ana_table$CAblue),"High","Low"),
+                            turquoise_exp = ifelse(CAturquoise >= median(ana_table$CAturquoise),"High","Low"))
+new$magenta_exp <- factor(new$magenta_exp)
+new$black_exp <- factor(new$black_exp)
+new$brown_exp <- factor(new$brown_exp)
+new$green_exp <- factor(new$green_exp)
+new$red_exp <- factor(new$red_exp)
+new$yellow_exp <- factor(new$yellow_exp)
+new$pink_exp <- factor(new$pink_exp)
+new$blue_exp <- factor(new$blue_exp)
+new$turquoise_exp <- factor(new$turquoise_exp)
+
+fit1 <- survfit(Surv(OS.time, OS) ~ magenta_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_magenta")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ black_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_black")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ brown_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_brown")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ green_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_green")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ red_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_red")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ yellow_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_yellow")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ pink_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_pink")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ blue_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_blue")
+
+fit1 <- survfit(Surv(OS.time, OS) ~ turquoise_exp, data = new)
+ggsurvplot(fit1, data = new, pval = TRUE,title = "Genus community_turquoise")
